0777ea309544a52df41f1762020e17de692724e7
elloramir <cripboygit@gmail.com>
2022-12-26 20:46:11 -0300

    first input component version

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Panel from "./Panel.svelte"
 	import Button from "./Button.svelte"
+	import Input from "./Input.svelte"
 </script>
 
 <Button />
+<Input />
 <Panel />
---
+++ src/components/Input.svelte
@@ -0,0 +1,26 @@
+<div class="input">
+	<div class="input-inner">
+		<input value="hello iam input" spellcheck="false" >
+	</div>
+</div>
+
+<style>
+	.input {
+		background-color: var(--tertiary);
+		border: 2px solid var(--outline);
+		border-radius: 3px;
+	}
+
+	.input-inner {
+		border: 2px solid var(--innerline);
+		padding: 5px 5px;
+	}
+
+	.input input {
+		width: 100%;
+		border: none;
+		outline: none;
+		background-color: transparent;
+		color: inherit;
+	}
+</style>
